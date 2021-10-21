b11088d170ede6c7693af51b5f02662ad1b17c8f
Salar Einavi Pour <salareinavipour@gmail.com>
2021-10-21 17:29:50 +0330

    Remove validation webhook

--- app/main.py
+++ app/main.py
@@ -2,15 +2,6 @@ from flask import Flask, request, jsonify
 
 admission_controller = Flask(__name__)
 
-@admission_controller.route('/validate/deployments', methods=['POST'])
-def validate_webhook():
-    req_info = request.get_json()["request"]["object"]["metadata"]
-
-    if req_info["labels"].get("env"):
-        return admission_response(True, "%s will be deployed in %s environment." % (req_info["name"], req_info["labels"].get("env")))
-    
-    return admission_response(False, "Label `env` is not defined for application %s" % req_info["name"])
-
 @admission_controller.route('/mutate/deployments', methods=['POST'])
 def mutate_webhook():
     req_info = request.get_json()["request"]["object"]["metadata"]
@@ -29,9 +20,6 @@ def admission_response_patch(message, json_patch):
     return jsonify({"response": {"status": {"message": message},
                                  "patchType": "JSONPatch",
                                  "patch": base64_patch}})
-
-def admission_response(allowed, message):
-    return jsonify({"response": {"allowed": allowed, "status": {"message": message}}})
 
 if __name__ == '__main__':
     admission_controller.run(host='0.0.0.0', port=443, ssl_context=("./certs/server.crt", "./certs/server.key"))
