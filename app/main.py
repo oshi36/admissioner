3ce75f2df3ea9ef10362a8f107dff75130b92a39
Salar Einavi Pour <salareinavipour@gmail.com>
2021-10-23 01:21:26 +0330

    Update main.py to only validate if label exists

--- app/main.py
+++ app/main.py
@@ -7,13 +7,13 @@ admission_controller = Flask(__name__)
 def deployment_webhook():
     req_info = request.get_json()["request"]["object"]["metadata"]
 
-    if req_info["labels"].get("env"):
-        return admission_response(True, "%s will be deployed in %s environment." % (req_info["name"], req_info["labels"].get("env")))
+    if req_info["labels"].get("production"):
+        return admission_response(True, "Deployment %s has `production` label." % req_info["name"])
     
-    return admission_response(False, "Label `env` is not defined for application %s" % req_info["name"])
+    return admission_response(False, "Deployment %s doesn't have `production` label." % req_info["name"])
 
 def admission_response(allowed, message):
     return jsonify({"response": {"allowed": allowed, "status": {"message": message}}})
 
 if __name__ == '__main__':
-    admission_controller.run(host='0.0.0.0', port=443, ssl_context=("/server.crt", "/server.key"))
+    admission_controller.run(host='0.0.0.0', port=443, ssl_context=("./certs/server.crt", "./certs/server.key"))
