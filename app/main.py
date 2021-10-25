9f358b57444e5e8dc6a03e5cf5caffb9455e6049
Salar Einavi Pour <salareinavipour@gmail.com>
2021-10-25 12:49:26 +0330

    Update main.py to use f-strings

--- app/main.py
+++ app/main.py
@@ -9,11 +9,11 @@ def deployment_webhook():
 
     if req_info["labels"].get("production"):
         if req_info["labels"]["production"] != "true" or req_info["labels"]["production"] != "false":
-            return admission_response(False, "Invalid value for `production` label in Deployment %s" % req_info["name"])
-        return admission_response(True, "Deployment %s has `production` label." % req_info["name"])
+            return admission_response(False, f"Invalid value for `production` label in Deployment {req_info['name']}")
+        return admission_response(True, f"Deployment {req_info['name']} has `production` label.")
         
     
-    return admission_response(False, "Deployment %s doesn't have `production` label." % req_info["name"])
+    return admission_response(False, f"Deployment {req_info['name']} doesn't have `production` label.")
 
 def admission_response(allowed, message):
     return jsonify({"response": {"allowed": allowed, "status": {"message": message}}})
