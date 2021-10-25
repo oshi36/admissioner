31873e32b179f9a14695a19d8e317d408bccc6ba
Salar Einavi Pour <salareinavipour@gmail.com>
2021-10-25 12:53:09 +0330

    Update main.py to add doc-string

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ admission_controller = Flask(__name__)
 @admission_controller.route('/validate/deployments', methods=['POST'])
 
 def deployment_webhook():
+    """Handler for the validate route."""
     req_info = request.get_json()["request"]["object"]["metadata"]
 
     if req_info["labels"].get("production"):
@@ -16,7 +17,9 @@ def deployment_webhook():
     return admission_response(False, f"Deployment {req_info['name']} doesn't have `production` label.")
 
 def admission_response(allowed, message):
+    """Create admission response."""
     return jsonify({"response": {"allowed": allowed, "status": {"message": message}}})
 
 if __name__ == '__main__':
+    """Running flask server with TLS certs"""
     admission_controller.run(host='0.0.0.0', port=443, ssl_context=("./certs/server.crt", "./certs/server.key"))
